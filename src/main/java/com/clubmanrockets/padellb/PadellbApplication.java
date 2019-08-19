9dfee0eee99c1b71336371260ffb89c5bcd94029
sergio.martin <sergio.martin@bestsecret.com>
2019-08-19 14:57:22 +0200

    Initial commit

---
+++ src/main/java/com/clubmanrockets/padellb/PadellbApplication.java
@@ -0,0 +1,13 @@
+package com.clubmanrockets.padellb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadellbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadellbApplication.class, args);
+	}
+
+}
